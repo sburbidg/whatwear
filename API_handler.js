f01ebe37f7acd935361c0c9581eb4434cb4ad839
Scott Burbidge <scottisreally@gmail.com>
2014-11-16 14:14:03 -0800

    got working

--- API_handler.js
+++ API_handler.js
@@ -2,23 +2,27 @@
 'use strict';
 
 var express = require('express');
-var app = express;
+var app = express();
 var bodyParser = require('body-parser');
 var request = require('superagent');
 
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.get('/', function(req, res) {
-  var getTemp = 'http://api.wunderground.com/api/' +
+  var wunderURL = 'http://api.wunderground.com/api/' +
   '924831b5dccbad67' + '/geolookup/conditions/forecast/q/' +
   'autoip' +
   '.json';
 
   request
-  app.get(getTemp);
-  app.end (function(err, getTempData) {
-    if (err) console.log('there was an error');
-    var tempParse = JSON.parse(getTempData.text);
-    res.json({temp: tempParse.current_observation.temp_f});
-  });
+    .get(wunderURL)
+    .end (function(err, wunderData) {
+      if (err) console.log('there was an error');
+      var tempParse = JSON.parse(wunderData.text);
+      res.json({temp: tempParse.current_observation.temp_f});
+    });
 });
 
 app.set('port', process.env.PORT || 3000);
